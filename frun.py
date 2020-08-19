5a7b9522fa3446767b203db9dab98ad7459a4dd9
Tan Tran <2188362+tant@users.noreply.github.com>
2020-08-19 12:43:12 +0700

    Update frun.py
    Điều chỉnh lại các công thức tính dễ hiểu hơn

--- frun.py
+++ frun.py
@@ -32,13 +32,15 @@
 #    <trkseg>
 
 import xml.etree.ElementTree as ET
-import geopy.distance
+from geopy import distance
 import math
 import random
 import datetime
 
 tree = ET.parse('input.xml')
 trkseg = tree.getroot()[0][0]
+
+
 
 p0 = trkseg[0]
 lat0 = p0.attrib['lat']
@@ -48,51 +50,63 @@ time0 = p0[1].text
 i = 0
 
 for p in trkseg:
-	if i==0 : 
-		i = i+1
-		continue
-	else:
-		# Tính khoảng cách với tọa độ trước, từ đó tính ra thời gian cần để hoàn thành
-		c1 = (p.attrib['lat'], p.attrib['lon'])
-		dist = geopy.distance.vincenty(c0, c1).m
-		# số giây + khoảng ngẫu nhiên 
-		duration = dist *(310 + random.randrange(-40,40,2))/1000
-		duration = round(duration,1)
-		# Cộng thời gian trước với thời gian hoàn thành để ra thời điểm hoàn thành
-		dt0 = datetime.datetime.fromisoformat(time0[:-1])
-		td = datetime.timedelta(seconds=duration)
-		dt1 = dt0+td
-		time1 = dt1.isoformat() + 'Z'
-		# cập nhật lại thời điểm hoàn thành vào dữ liệu
-		p[1].text= time1
-		# Thêm dữ liệu mở rộng
-		ext = ET.Element('extensions')
-		tpe = ET.SubElement(ext, 'gpxtpx:TrackPointExtension')
-		temp = ET.SubElement(tpe, 'gpxtpx:atemp')
-		temp.text = '30'
-		hr = ET.SubElement(tpe, 'gpxtpx:hr')
-		hr.text = str(random.randint(143,158))
-		cad = ET.SubElement(tpe, 'gpxtpx:cad')
-		cad.text = str(random.randint(58,69))
-		p.append(ext)
-		# độ thêm mức chính xác của tọa độ
-		for j in range(23):
-			p.attrib['lat'] = p.attrib['lat'] + str(random.randint(0,9))
-			p.attrib['lon'] = p.attrib['lon'] + str(random.randint(0,9))
-		# cập nhật lại dữ liệu mới
-		c0 = c1
-		time0 = time1
-		i = i + 1
+    if i==0 : 
+        i = i+1
+        continue
+    else:
+        # Tính khoảng cách với tọa độ trước đơn vị ra met
+        c1 = (p.attrib['lat'], p.attrib['lon'])
+        dist = distance.distance(c0, c1).m
+        # Tính thời gian hoàn thành khoảng cách trên
+        #   Tính vận tốc trung bình theo pace.
+        # EDIT
+        min4km = 1.82 # số phút hoàn thành 1km
+        #   Tính vận tốc theo đơn vị m/s
+        velocity = 1000 / (min4km*60)
+        #   Gia giảm ngẫu nhiên chút
+        # 1m/s = 60m/m = 3600m/h = 3.6kmh
+        # randrange không chấp nhận số thực nên chia xuống nhân lên với 1000 xong xíu chia xuống
+        # EDIT
+        vstart = -1000
+        vend = +1000
+        vstep = 200
+        
+        velocity = velocity + random.randrange(vstart,vend,vstep)/10000
+        #   số giây 
+        duration = round(dist / velocity , 1)
+        
+        # Cộng thời gian trước với thời gian hoàn thành để ra thời điểm hoàn thành
+        dt0 = datetime.datetime.fromisoformat(time0[:-1])
+        td = datetime.timedelta(seconds=duration)
+        dt1 = dt0+td
+        time1 = dt1.isoformat() + 'Z'
+        # cập nhật lại thời điểm hoàn thành vào dữ liệu
+        p[1].text= time1
+        
+        
+        # Thêm dữ liệu mở rộng
+        ext = ET.Element('extensions')
+        tpe = ET.SubElement(ext, 'gpxtpx:TrackPointExtension')
+        #  Nhiệt độ
+        temp = ET.SubElement(tpe, 'gpxtpx:atemp')
+        temp.text = '30'
+        # Nhịp tim
+        # EDIT: Range tim
+        hr = ET.SubElement(tpe, 'gpxtpx:hr')
+        hr.text = str(random.randint(167,179))
+        # Cadence
+        # EDIT: Range cadence
+        cad = ET.SubElement(tpe, 'gpxtpx:cad')
+        cad.text = str(random.randint(79,92))
+        p.append(ext)
+        # độ thêm mức chính xác của tọa độ
+        for j in range(23):
+            p.attrib['lat'] = p.attrib['lat'] + str(random.randint(0,9))
+            p.attrib['lon'] = p.attrib['lon'] + str(random.randint(0,9))
+        # cập nhật lại dữ liệu mới
+        c0 = c1
+        time0 = time1
+        i = i + 1
 
 tree.write('output.xml')
-
-
-
-
-
-
-
-
-
-
 
